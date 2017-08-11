695885dea2db309d74c4254444ac852c5d554f60
Filippo Poltronieri <fpoltronieri@ihmc.us>
2017-08-11 11:24:19 -0500

    ok

--- app/src/main/java/us/ihmc/dataviewer/MainActivity.kt
+++ app/src/main/java/us/ihmc/dataviewer/MainActivity.kt
@@ -3,7 +3,6 @@ package us.ihmc.dataviewer
 import android.Manifest
 import android.app.Activity
 import android.content.Intent
-import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
